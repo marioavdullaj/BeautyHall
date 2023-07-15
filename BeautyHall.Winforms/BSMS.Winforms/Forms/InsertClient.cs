bf95516b3e48ecf2578c04dbdf83548c4d294df7
PX <PX@MANDISPRO11>
2023-07-15 17:39:29 +0300

    UI Changes

--- BeautyHall.Winforms/BSMS.Winforms/Forms/InsertClient.cs
+++ BeautyHall.Winforms/BSMS.Winforms/Forms/InsertClient.cs
@@ -17,5 +17,25 @@ namespace BSMS.Winforms.Forms
         {
             InitializeComponent();
         }
+
+        protected override void WndProc(ref Message message)
+        {
+            const int WM_SYSCOMMAND = 0x0112;
+            const int SC_MOVE = 0xF010;
+            const int SC_MAXIMIZE = 0xF030;
+            const int SC_MINIMIZE = 0xF020;
+
+            switch (message.Msg)
+            {
+                case WM_SYSCOMMAND:
+                    int command = message.WParam.ToInt32() & 0xfff0;
+                    if (command == SC_MOVE || command == SC_MAXIMIZE || command == SC_MINIMIZE)
+                        return;
+                    break;
+            }
+
+            base.WndProc(ref message);
+        }
     }
+
 }
