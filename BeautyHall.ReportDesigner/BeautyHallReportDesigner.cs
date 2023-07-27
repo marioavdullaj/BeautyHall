deddfd2aa3a2e72f95d2a70bebd1e1bc8bf97eec
developer <developer@stesi.it>
2023-07-27 15:39:59 +0200

    Report designer project introduced

---
+++ BeautyHall.ReportDesigner/BeautyHallReportDesigner.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using DevExpress.XtraBars;
+namespace BeautyHall.ReportDesigner
+{
+    public partial class BeautyHallReportDesigner : DevExpress.XtraBars.Ribbon.RibbonForm
+    {
+        public BeautyHallReportDesigner() => InitializeComponent();
+        private void btnConnect_ItemClick(object sender, ItemClickEventArgs e) { }
+        private void BeautyHallReportDesigner_Load(object sender, EventArgs e) { }
+    }
+}
